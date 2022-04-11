fce2c6045e560e65afc4c78c996c74cf6985c437
brunodominguez1303 <bruno.dominguez1303@gmail.com>
2022-04-11 00:21:45 -0300

    code refactoration

--- config/db.js
+++ config/db.js
@@ -1,11 +1,5 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://brunodominguez:<password>@bloagapp.o6yos.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
 /* Verifies where the application is running, local or an outside server */
 
 if(process.env.NODE_ENV == 'production') {
@@ -18,3 +12,12 @@ if(process.env.NODE_ENV == 'production') {
 		mongoURI: "mongodb://localhost/blogapp"
 	}
 }
+
+/*const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://brunodominguez:<password>@bloagapp.o6yos.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});*/
